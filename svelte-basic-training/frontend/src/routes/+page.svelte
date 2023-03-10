39b026a6d811e4e8caf96c6f7852370dee26ab4e
Eric-JongYoung <fgh1937@yahoo.com>
2023-03-10 22:24:42 +0900

    Assignment - 1st
    첫번째 과제
    [Backend]
    - Autoflow Pro 이용하여 GET 방식으로 /board/list 에 json 형식으로 DB 반환
    - 해당 파일은 AutoFlow 폴더에 config.json 참조
    [Frontend]
    - Svelte 을 이용하여 /board 경로에서 DB정보 반환
    - 응답 데이터를 리스트로 표기되도록 작업
    - 등록일자 기준 내림차순 정렬
    - 리스트 15개로 페이징 작업

---
+++ svelte-basic-training/frontend/src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<div class="main">
+		<img src="/src/lib/images/basic_logo.png" alt="SvelteKit" class="s-Cmt25qOMERl7">
+		<div class="main-content">
+		고종영<br>
+		1번째 과제 내용 입니다.<br>
+		감사합니다.
+		</div>
+	</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+	.main{
+		text-align: center;
+	}
+	.main-content{
+		font-family: 'LINESeedKR-Bd';
+	}
+</style>
---
+++ svelte-basic-training/frontend/src/routes/board/+page.svelte
@@ -0,0 +1,101 @@
+<script>
+	import { onMount } from "svelte";
+	let my = []; // 빈 배열로 초기화
+	let currentPage = 1;
+	let itemsPerPage = 10; // 페이지 당 아이템 수
+	let totalPages = 0;
+
+	// ajax
+	
+	const fetchData = async () => {
+		const response = await fetch(`http://localhost:8000/board/list`);
+		const data = await response.json();
+		my = data;
+		totalPages = Math.ceil(my.length / itemsPerPage);
+	};
+
+	onMount(fetchData); // 컴포넌트가 마운트될 때 데이터를 가져옴
+
+	function setPage(page) {
+		 currentPage = page;
+
+	}
+
+	function getPaginatedItems(now) {
+		const startIndex = (now - 1) * itemsPerPage;
+		const endIndex = startIndex + itemsPerPage;
+		return my.slice(startIndex, endIndex);
+	}
+
+	function getPages() {
+		const pages = [];
+		for (let i = 1; i <= totalPages; i++) {
+			pages.push(i);
+		}
+		return pages;
+	}
+	function changePage(next){
+		if(next==1){
+			currentPage ++;
+		}else{
+			currentPage --;
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<table>
+		<thead>
+			<tr>
+				<th style="width: 30px;">ID</th>
+				<th>Title</th>
+				<th>Content</th>
+				<th style="width: 100px;">Date</th>
+			</tr>
+		</thead>
+		{#await my}
+			<p>...Loading</p>
+		{:then my}
+			{#if my.length === 0}
+				<p>...Loading</p>
+			{:else}
+				<tbody>
+					{#each getPaginatedItems(currentPage) as item, index}
+						<tr>
+							<td style="text-align: center">{item.id}</td>
+							<td>{item.title}</td>
+							<td>{item.content}</td>
+							<td>
+								{new Date(item.create_date)
+									.toISOString()
+									.slice(0, 10)}
+							</td>
+						</tr>
+					{/each}
+				</tbody>
+			{/if}
+		{:catch error}
+			<p>ajax 오류가 발생했습니다.</p>
+		{/await}
+	</table>
+	{#if totalPages > 1}
+		<div class="page-num">
+			{#if currentPage>1}
+			<a href="#" on:click={() => changePage(0)}>&laquo;</a>
+			{/if}
+			{#each getPages() as page}
+				<span class:active={currentPage === page}>
+					<a href="#" on:click={() => setPage(page)}>{page}</a>
+				</span>
+			{/each}
+			{#if currentPage<totalPages}
+			<a href="#" on:click={() => changePage(1)}>&raquo;</a>
+			{/if}
+		</div>
+	{/if}
+</div>
